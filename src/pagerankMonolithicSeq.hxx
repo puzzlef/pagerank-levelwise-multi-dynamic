d807502e3d51c93bd05dedf629c43f1aadc5ce95
Subhajit Sahu <wolfram77@gmail.com>
2021-11-07 23:29:06 +0530

    :skunk: w/ pagerankMonolithicSeq

--- src/pagerankMonolithicSeq.hxx
+++ src/pagerankMonolithicSeq.hxx
@@ -5,6 +5,8 @@
 #include "vertices.hxx"
 #include "edges.hxx"
 #include "csr.hxx"
+#include "transpose.hxx"
+#include "dynamic.hxx"
 #include "pagerank.hxx"
 
 using std::vector;
@@ -35,59 +37,98 @@ void pagerankCalculate(vector<T>& a, const vector<T>& c, const vector<int>& vfro
 // --------------
 
 template <class T>
-T pagerankError(const vector<T>& x, const vector<T>& y, int i, int N, int EF) {
+T pagerankError(const vector<T>& x, const vector<T>& y, int i, int n, int EF) {
   switch (EF) {
-    case 1:  return l1Norm(x, y, i, N);
-    case 2:  return l2Norm(x, y, i, N);
-    default: return liNorm(x, y, i, N);
+    case 1:  return l1Norm(x, y, i, n);
+    case 2:  return l2Norm(x, y, i, n);
+    default: return liNorm(x, y, i, n);
   }
 }
 
 
 
 
-// PAGERANK-LOOP
-// --------------
+// PAGERANK-CORE
+// -------------
 
 template <class T>
-int pagerankSeqLoop(vector<T>& a, vector<T>& r, vector<T>& c, const vector<T>& f, const vector<int>& vfrom, const vector<int>& efrom, int i, int n, int N, T p, T E, int L, int EF) {
+int pagerankMonolithicSeqLoop(vector<T>& a, vector<T>& r, vector<T>& c, const vector<T>& f, const vector<int>& vfrom, const vector<int>& efrom, int i, int n, int N, T p, T E, int L, int EF) {
   T  c0 = (1-p)/N;
   int l = 0;
-  for (; l<L; ++l) {
-    multiply(c, r, f, i, n);
-    pagerankCalculate(a, c, vfrom, efrom, i, n, c0);
-    T el = pagerankError(a, r, i, n, EF);
-    if (el < E) { ++l; break; }
-    swap(a, r);
+  while (l<L) {
+    pagerankCalculate(a, c, vfrom, efrom, i, n, c0);  // assume contribtions (c) is precalculated
+    T el = pagerankError(a, r, i, n, EF); ++l;        // one iteration complete
+    if (el<E || l>=L) break;                          // check tolerance, iteration limit
+    multiply(c, a, f, i, n);                          // update partial contributions (c)
+    swap(a, r);                                       // final ranks always in (a)
   }
   return l;
 }
 
 
-// Find pagerank using a single thread (pull, CSR).
-// @param xt transpose graph, with vertex-data=out-degree
-// @param q initial ranks (optional)
-// @param o options {damping=0.85, tolerance=1e-6, maxIterations=500}
-// @returns {ranks, iterations, time}
-template <class H, class T=float>
-PagerankResult<T> pagerankSeq(const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+template <class H, class J, class FL, class T=float>
+PagerankResult<T> pagerankMonolithicSeqCore(const H& xt, const J&& ks, int i, int n, FL fl, const vector<T> *q, PagerankOptions<T> o) {
+  int  N  = xt.order();
   T    p  = o.damping;
   T    E  = o.tolerance;
   int  L  = o.maxIterations, l = 0;
   int  EF = o.toleranceNorm;
-  auto ks    = vertices(xt);
-  auto vfrom = sourceOffsets(xt);
-  auto efrom = destinationIndices(xt);
-  auto vdata = vertexData(xt);
-  int  N     = xt.order();
+  auto vfrom = sourceOffsets(xt, ks);
+  auto efrom = destinationIndices(xt, ks);
+  auto vdata = vertexData(xt, ks);
   vector<T> a(N), r(N), c(N), f(N), qc;
-  if (q) qc = compressContainer(xt, *q);
+  if (q) qc = compressContainer(xt, *q, ks);
   float t = measureDurationMarked([&](auto mark) {
-    if (q) copy(r, qc);
+    if (N==0 || n==0) return;  // skip if nothing to do!
+    if (q) copy(r, qc);        // copy old ranks (qc), if given
     else fill(r, T(1)/N);
     copy(a, r);
-    mark([&] { pagerankFactor(f, vdata, 0, N, p); });
-    mark([&] { l = pagerankSeqLoop(a, r, c, f, vfrom, efrom, 0, N, N, p, E, L, EF); });
+    mark([&] { pagerankFactor(f, vdata, 0, N, p); multiply(c, a, f, 0, N); });  // calculate factors (f) and contributions (c)
+    mark([&] { l = fl(a, r, c, f, vfrom, efrom, 0, N, N, p, E, L, EF); });      // calculate ranks of all vertices
   }, o.repeat);
-  return {decompressContainer(xt, a), l, t};
+  return {decompressContainer(xt, a, ks), l, t};
+}
+
+
+
+
+// PAGERANK (STATIC / INCREMENTAL)
+// -------------------------------
+
+// Find pagerank using a single thread (pull, CSR).
+// @param x original graph
+// @param q initial ranks (optional)
+// @param o options {damping=0.85, tolerance=1e-6, maxIterations=500}
+// @returns {ranks, iterations, time}
+template <class G, class FL, class T=float>
+PagerankResult<T> pagerankMonolithicSeq(const G& x, FL fl, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  int  N  = x.order();
+  auto xt = transposeWithDegree(x);
+  return pagerankMonolithicSeqCore(xt, xt.vertices(), 0, N, fl, q, o);
+}
+
+template <class G, class T=float>
+PagerankResult<T> pagerankMonolithicSeq(const G& x, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  return pagerankMonolithicSeq(x, pagerankMonolithicSeqLoop<T>, q, o);
+}
+
+
+
+
+// PAGERANK (DYNAMIC)
+// ------------------
+
+template <class G, class FD, class FL, class T=float>
+PagerankResult<T> pagerankMonolithicSeqDynamic(const G& x, const G& y, FD fd, FL fl, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  auto [ks, n] = fd(x, y);
+  auto yt = transposeWithDegree(y);
+  return pagerankMonolithicSeqCore(yt, ks, 0, n, fl, q, o);
+}
+
+template <class G, class T=float>
+PagerankResult<T> pagerankMonolithicSeqDynamic(const G& x, const G& y, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  auto xt = transposeWithDegree(x);
+  auto yt = transposeWithDegree(y);
+  auto [ks, n] = dynamicVertices(x, xt, y, yt);
+  return pagerankMonolithicSeqCore(yt, ks, 0, n, pagerankMonolithicSeqLoop<T>, q, o);
 }
