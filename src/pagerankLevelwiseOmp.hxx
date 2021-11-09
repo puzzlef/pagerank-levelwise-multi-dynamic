57fb24abdceafdbf51f08eb71552854ab681ddd3
Subhajit Sahu <wolfram77@gmail.com>
2021-11-09 10:46:43 +0530

    :skunk: w/ pagerank levelwise seq, omp (program debt)

--- src/pagerankLevelwiseOmp.hxx
+++ src/pagerankLevelwiseOmp.hxx
@@ -4,10 +4,12 @@
 #include "vertices.hxx"
 #include "edges.hxx"
 #include "csr.hxx"
+#include "transpose.hxx"
 #include "components.hxx"
-#include "topologicalSort.hxx"
+#include "dynamic.hxx"
 #include "pagerank.hxx"
-#include "pagerankMonolithic.hxx"
+#include "pagerankOmp.hxx"
+#include "pagerankMonolithicOmp.hxx"
 
 using std::vector;
 using std::swap;
@@ -15,13 +17,10 @@ using std::swap;
 
 
 
-template <class G, class H>
-auto pagerankComponents(const G& x, const H& xt) {
-  auto a = components(x, xt);
-  auto b = blockgraph(x, a);
-  auto bks = topologicalSort(b);
-  reorder(a, bks);
-  return a;
+
+template <class T>
+inline T pagerankLevelwiseError(T E, int n, int N, int EF) {
+  return EF<=2? E*n/N : E;
 }
 
 
@@ -69,12 +68,12 @@ auto pagerankGroupWaves(const C& cs) {
 
 
 template <class T, class J>
-int pagerankLevelwiseLoop(vector<T>& a, vector<T>& r, vector<T>& c, const vector<T>& f, const vector<int>& vfrom, const vector<int>& efrom, int i, J&& ns, int N, T p, T E, int L) {
+int pagerankLevelwiseOmpLoop(vector<T>& a, vector<T>& r, vector<T>& c, const vector<T>& f, const vector<int>& vfrom, const vector<int>& efrom, int i, J&& ns, int N, T p, T E, int L, int EF) {
   float l = 0;
   for (int n : ns) {
-    float nN = float(n)/N;
     if (n<=0) { i += -n; continue; }
-    l += pagerankMonolithicLoop(a, r, c, f, vfrom, efrom, i, n, N, p, E*nN, L) * nN;
+    T np = T(n)/N, En = pagerankLevelwiseError(E, n, N, EF);
+    l += pagerankMonolithicOmpLoop(a, r, c, f, vfrom, efrom, i, n, N, p, En, L, EF)*np;
     swap(a, r);
     i += n;
   }
@@ -83,50 +82,51 @@ int pagerankLevelwiseLoop(vector<T>& a, vector<T>& r, vector<T>& c, const vector
 }
 
 
-// Find pagerank of components in topological order (pull, CSR, skip-comp).
-// @param w  previous graph
-// @param wt previous transpose graph, with vertex-data=out-degree
-// @param x  current graph
-// @param xt current transpose graph, with vertex-data=out-degree
-// @param q initial ranks (optional)
-// @param o options {damping=0.85, tolerance=1e-6, maxIterations=500}
+
+
+// PAGERANK (STATIC / INCREMENTAL)
+// -------------------------------
+
+// Find pagerank using a single thread (pull, CSR).
+// @param x  original graph
+// @param xt transpose graph (with vertex-data=out-degree)
+// @param q  initial ranks (optional)
+// @param o  options {damping=0.85, tolerance=1e-6, maxIterations=500}
 // @returns {ranks, iterations, time}
 template <class G, class H, class T=float>
-PagerankResult<T> pagerankLevelwise(const G& w, const H& wt, const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
-  T    p = o.damping;
-  T    E = o.tolerance;
-  int  L = o.maxIterations, l;
-  int  N = xt.order();
-  auto wcs = pagerankComponents(w, wt);
-  auto xcs = pagerankComponents(x, xt);
-  auto ws = pagerankWaves(w, wt, wcs, x, xt, xcs);
-  auto cs = pagerankGroupComponents(xcs, ws);
-  auto ns = pagerankGroupWaves(cs);
+PagerankResult<T> pagerankLevelwiseOmp(const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  int  N  = xt.order();
+  auto cs = joinUntilSize(sortedComponents(x, xt), MIN_COMPUTE_PR());
+  auto ns = pagerankLevelwiseWaves(cs);
   auto ks = join(cs);
-  auto vfrom = sourceOffsets(xt, ks);
-  auto efrom = destinationIndices(xt, ks);
-  auto vdata = vertexData(xt, ks);
-  vector<T> a(N), r(N), c(N), f(N), qc;
-  if (q) qc = compressContainer(xt, *q, ks);
-  float t = measureDurationMarked([&](auto mark) {
-    fill(a, T());
-    if (q) copy(r, qc);
-    else fill(r, T(1)/N);
-    mark([&] { pagerankFactor(f, vdata, 0, N, p); multiply(c, r, f); copy(a, r); });
-    mark([&] { l = pagerankLevelwiseLoop(a, r, c, f, vfrom, efrom, 0, ns, N, p, E, L); });
-  }, o.repeat);
-  return {decompressContainer(xt, a, ks), l, t};
+  return pagerankOmp(xt, ks, 0, ns, pagerankLevelwiseOmpLoop<T>, q, o);
+}
+template <class G, class T=float>
+PagerankResult<T> pagerankLevelwiseOmp(const G& x, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  auto xt = transposeWithDegree(x);
+  return pagerankLevelwiseOmp(x, xt, q, o);
 }
 
 
-// Find pagerank of components in topological order (pull, CSR).
-// @param x  current graph
-// @param xt current transpose graph, with vertex-data=out-degree
-// @param q initial ranks (optional)
-// @param o options {damping=0.85, tolerance=1e-6, maxIterations=500}
-// @returns {ranks, iterations, time}
+
+
+// PAGERANK (DYNAMIC)
+// ------------------
+
 template <class G, class H, class T=float>
-PagerankResult<T> pagerankLevelwise(const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
-  DiGraph<> w; DiGraph<int> wt;
-  return pagerankLevelwise(w, wt, x, xt, q, o);
+PagerankResult<T> pagerankLevelwiseOmpDynamic(const G& x, const H& xt, const G& y, const H& yt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  auto cs = sortedComponents(y, yt);
+  auto b  = blockgraph(x, cs);
+  auto [ics, nc] = dynamicComponentIndices(x, y, cs, b);
+  if (nc==0) return PagerankResult<T>::initial(xt, q);
+  auto cs = pagerankLevelwiseGroupComponents();  // PROGRAM DEBT!
+  auto ns = pagerankLevelwiseGroupWaves();       // PROGRAM DEBT!
+  auto ks = join(cs);
+  return pagerankOmp(xt, ks, 0, ns, pagerankLevelwiseOmpLoop<T>, q, o);
+}
+template <class G, class T=float>
+PagerankResult<T> pagerankLevelwiseOmpDynamic(const G& x, const G& y, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+  auto xt = transposeWithDegree(x);
+  auto yt = transposeWithDegree(y);
+  return pagerankLevelwiseOmpDynamic(x, xt, y, yt, q, o);
 }
