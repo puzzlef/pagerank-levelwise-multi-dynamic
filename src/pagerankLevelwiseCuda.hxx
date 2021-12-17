9f9719eaa02d3054f56ab2afd26b01d949266e2b
Subhajit Sahu <wolfram77@gmail.com>
2021-12-17 18:38:11 +0530

    :turtle: w/ pagerank levelwise cuda

--- src/pagerankLevelwiseCuda.hxx
+++ src/pagerankLevelwiseCuda.hxx
@@ -13,31 +13,11 @@
 #include "pagerank.hxx"
 #include "pagerankCuda.hxx"
 #include "pagerankMonolithicCuda.hxx"
+#include "pagerankComponentwiseCuda.hxx"
 
 using std::array;
 using std::vector;
 using std::swap;
-
-
-
-
-// PAGERANK-LOOP
-// -------------
-
-template <class T, class J>
-int pagerankComponentwiseCudaLoop(T *e, T *r0, T *eD, T *r0D, T *&aD, T *&rD, T *cD, const T *fD, const int *vfromD, const int *efromD, int i, const J& ws, int N, T p, T E, int L, int EF) {
-  float l = 0;
-  for (const auto& w : ws) {
-    const auto& [nt, nb] = w; int n = nt+nb;
-    if (n<=0) { i += -n; continue; }
-    T np = T(n)/N, En = EF<=2? E*n/N : E;
-    l += pagerankMonolithicCudaLoop(e, r0, eD, r0D, aD, rD, cD, fD, vfromD, efromD, i, w, N, p, En, L, EF)*np;
-    swap(aD, rD);
-    i += n;
-  }
-  swap(aD, rD);
-  return int(l);
-}
 
 
 
@@ -52,27 +32,27 @@ int pagerankComponentwiseCudaLoop(T *e, T *r0, T *eD, T *r0D, T *&aD, T *&rD, T 
 // @param o  options {damping=0.85, tolerance=1e-6, maxIterations=500}
 // @returns {ranks, iterations, time}
 template <class G, class H, class T=float>
-PagerankResult<T> pagerankComponentwiseCuda(const G& x, const H& xt, const vector<T> *q, const PagerankOptions<T>& o, const PagerankData<G>& D) {
+PagerankResult<T> pagerankLevelwiseCuda(const G& x, const H& xt, const vector<T> *q, const PagerankOptions<T>& o, const PagerankData<G>& D) {
   const auto& cs = D.components;
   const auto& b  = D.blockgraph;
   int  N  = xt.order();  if (N==0) return PagerankResult<T>::initial(xt, q);
-  auto ds = topologicalComponentsFrom(cs, b);
-  auto gs = joinUntilSize(ds, o.minCompute);
+  auto bt = transpose(b);
+  auto gs = levelwiseGroupedComponentsFrom(cs, bt);
   forEach(gs, [&](auto& g) { pagerankPartition(xt, g); });
   auto ns = pagerankPairWave(xt, gs);
   auto ks = join(gs);
   return pagerankCuda(xt, ks, 0, ns, pagerankComponentwiseCudaLoop<T, decltype(ns)>, q, o);
 }
 template <class G, class H, class T=float>
-PagerankResult<T> pagerankComponentwiseCuda(const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+PagerankResult<T> pagerankLevelwiseCuda(const G& x, const H& xt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
   auto cs = components(x, xt);
   auto b  = blockgraph(x, cs);
-  return pagerankComponentwiseCuda(x, xt, q, o, PagerankData<G>(cs, b));
+  return pagerankLevelwiseCuda(x, xt, q, o, PagerankData<G>(cs, b));
 }
 template <class G, class T=float>
-PagerankResult<T> pagerankComponentwiseCuda(const G& x, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+PagerankResult<T> pagerankLevelwiseCuda(const G& x, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
   auto xt = transposeWithDegree(x);
-  return pagerankComponentwiseCuda(x, xt, q, o);
+  return pagerankLevelwiseCuda(x, xt, q, o);
 }
 
 
@@ -82,27 +62,31 @@ PagerankResult<T> pagerankComponentwiseCuda(const G& x, const vector<T> *q=nullp
 // ------------------
 
 template <class G, class H, class T=float>
-PagerankResult<T> pagerankComponentwiseCudaDynamic(const G& x, const H& xt, const G& y, const H& yt, const vector<T> *q, const PagerankOptions<T>& o, const PagerankData<G>& D) {
+PagerankResult<T> pagerankLevelwiseCudaDynamic(const G& x, const H& xt, const G& y, const H& yt, const vector<T> *q, const PagerankOptions<T>& o, const PagerankData<G>& D) {
   const auto& cs = D.components;
   const auto& b  = D.blockgraph;
   int  N  = yt.order();                                 if (N==0) return PagerankResult<T>::initial(yt, q);
-  auto ds = topologicalComponentsFrom(cs, b);
+  auto bt = transpose(b);
+  auto ds = levelwiseComponentsFrom(cs, bt):
+  auto gi = levelwiseGroupIndices(bt);
   auto [is, n] = dynamicComponentIndices(x, y, ds, b);  if (n==0) return PagerankResult<T>::initial(yt, q);
-  auto gs = joinAtUntilSize(ds, sliceIter(is, 0, n), o.minCompute);
+  auto fn = [&](const auto& ig, int i) { return ig.empty() || ig.back()==i; };
+  auto ig = joinIf(sliceIter(is, 0, n), fn);
+  auto gs = joinAt2d(ds, ig);
   forEach(gs, [&](auto& g) { pagerankPartition(yt, g); });
   auto ns = pagerankPairWave(yt, gs);
-  auto ks = join(gs); joinAt(gs, ds, sliceIter(is, n));
+  auto ks = join(gs); joinAt(ks, ds, sliceIter(is, n));
   return pagerankCuda(yt, ks, 0, ns, pagerankComponentwiseCudaLoop<T, decltype(ns)>, q, o);
 }
 template <class G, class H, class T=float>
-PagerankResult<T> pagerankComponentwiseCudaDynamic(const G& x, const H& xt, const G& y, const H& yt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+PagerankResult<T> pagerankLevelwiseCudaDynamic(const G& x, const H& xt, const G& y, const H& yt, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
   auto cs = components(y, yt);
   auto b  = blockgraph(y, cs);
-  return pagerankComponentwiseCudaDynamic(x, xt, y, yt, q, o, PagerankData<G>(cs, b));
+  return pagerankLevelwiseCudaDynamic(x, xt, y, yt, q, o, PagerankData<G>(cs, b));
 }
 template <class G, class T=float>
-PagerankResult<T> pagerankComponentwiseCudaDynamic(const G& x, const G& y, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
+PagerankResult<T> pagerankLevelwiseCudaDynamic(const G& x, const G& y, const vector<T> *q=nullptr, PagerankOptions<T> o={}) {
   auto xt = transposeWithDegree(x);
   auto yt = transposeWithDegree(y);
-  return pagerankComponentwiseCudaDynamic(x, xt, y, yt, q, o);
+  return pagerankLevelwiseCudaDynamic(x, xt, y, yt, q, o);
 }
