a5dd0dbd9531c88c87917b226e7ec0fb06e53bb7
Subhajit Sahu <wolfram77@gmail.com>
2022-01-26 10:37:54 +0530

    :turtle: w/ expt

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-src="kitchen-sink"
+src="pagerank-multi-monolithic-vs-levelwise"
 out="/home/resources/Documents/subhajit/$src.log"
 ulimit -s unlimited
 printf "" > "$out"
@@ -12,19 +12,13 @@ cd $src
 # Run
 nvcc -std=c++17 -Xcompiler -fopenmp -lnvgraph -O3 main.cu
 stdbuf --output=L ./a.out ~/data/web-Stanford.mtx      2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/web-BerkStan.mtx      2>&1 | tee -a "$out"
 stdbuf --output=L ./a.out ~/data/web-Google.mtx        2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/web-NotreDame.mtx     2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/soc-Slashdot0811.mtx  2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/soc-Slashdot0902.mtx  2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/soc-Epinions1.mtx     2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/coAuthorsDBLP.mtx     2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/coAuthorsCiteseer.mtx 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/soc-LiveJournal1.mtx  2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/coPapersCiteseer.mtx  2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/coPapersDBLP.mtx      2>&1 | tee -a "$out"
 stdbuf --output=L ./a.out ~/data/indochina-2004.mtx    2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/data/soc-Epinions1.mtx     2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/data/soc-LiveJournal1.mtx  2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/data/wiki-Talk.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/data/cit-Patents.mtx       2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/data/coPapersDBLP.mtx      2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/data/amazon-2008.mtx       2>&1 | tee -a "$out"
 stdbuf --output=L ./a.out ~/data/italy_osm.mtx         2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/great-britain_osm.mtx 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/germany_osm.mtx       2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/data/asia_osm.mtx          2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/data/Linux_call_graph.mtx  2>&1 | tee -a "$out"
