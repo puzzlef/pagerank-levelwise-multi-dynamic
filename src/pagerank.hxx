5107428515f32ba59f74e5c52868ea7c60e21117
Subhajit Sahu <wolfram77@gmail.com>
2023-12-10 21:24:28 +0530

    :bug: update pagerank parameters (to match)

--- src/pagerank.hxx
+++ src/pagerank.hxx
@@ -66,7 +66,7 @@ struct PagerankOptions {
   T    tolerance;
   int  maxIterations;
 
-  PagerankOptions(int repeat=1, int toleranceNorm=1, int minCompute=1, bool splitComponents=false, T damping=0.85, T tolerance=1e-6, int maxIterations=500) :
+  PagerankOptions(int repeat=1, int toleranceNorm=3, int minCompute=1, bool splitComponents=false, T damping=0.85, T tolerance=1e-10, int maxIterations=500) :
   repeat(repeat), toleranceNorm(toleranceNorm), minCompute(minCompute), splitComponents(splitComponents), damping(damping), tolerance(tolerance), maxIterations(maxIterations) {}
 };
 
