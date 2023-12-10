40ea84b5ee34ceadbde5f3019900e1549cf244c7
Subhajit Sahu <wolfram77@gmail.com>
2023-12-10 21:20:47 +0530

    :bug: update graphs to run on

--- main.sh
+++ main.sh
@@ -13,17 +13,19 @@ fi
 
 # Run
 nvcc -std=c++17 -Xcompiler -fopenmp -O3 main.cu
-stdbuf --output=L ./a.out ~/Data/arabic-2005.mtx       2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/uk-2005.mtx           2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/it-2004.mtx           2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/soc-Epinions1.mtx     2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/soc-LiveJournal1.mtx  2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/wiki-Talk.mtx         2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/cit-Patents.mtx       2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/coPapersDBLP.mtx      2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/amazon-2008.mtx       2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/italy_osm.mtx         2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/Linux_call_graph.mtx  2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/indochina-2004.mtx  2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/uk-2002.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/arabic-2005.mtx     2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/uk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/webbase-2001.mtx    2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/it-2004.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sk-2005.mtx         2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-LiveJournal.mtx 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/com-Orkut.mtx       2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/asia_osm.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/europe_osm.mtx      2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/kmer_A2a.mtx        2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/kmer_V1r.mtx        2>&1 | tee -a "$out"
 
 # Signal completion
 curl -X POST "https://maker.ifttt.com/trigger/puzzlef/with/key/${IFTTT_KEY}?value1=$src$1"
